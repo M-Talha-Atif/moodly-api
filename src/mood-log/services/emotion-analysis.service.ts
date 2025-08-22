0e9f74acaa8f823b85c32de4d967a033f74fb5f0
talha atif <talhaatif573@gmail.com>
2025-08-23 02:15:57 +0500

    increased timeout for emotion analysis service

--- src/mood-log/services/emotion-analysis.service.ts
+++ src/mood-log/services/emotion-analysis.service.ts
@@ -64,7 +64,7 @@ export class EmotionAnalysisService {
 
       const response = await axios.post(apiUrl, formData, {
         headers: formData.getHeaders(),
-        timeout: 15000,
+        timeout: 35000,
       });
 
       return ResultDto.ok<EmotionApiResponse>(
