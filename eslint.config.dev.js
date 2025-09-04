870f43a924986dcb0a46a61ba3a8c81c3c7ebc86
talha atif <talhaatif573@gmail.com>
2025-09-04 22:49:30 +0500

    `Removed unused imports and deleted unused files and documented whole project under docs folder

--- eslint.config.dev.js
+++ eslint.config.dev.js
@@ -2,7 +2,7 @@
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
-import tseslint from 'typescript-eslint'; // 👈 ye missing tha
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
