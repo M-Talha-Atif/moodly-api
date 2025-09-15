658498ffe264532884ef2679c04c3372a51ca9f9
talha atif <talhaatif573@gmail.com>
2025-09-15 20:32:03 +0500

    feat: make API URL configurable and add TypeORM documentation
    - Replace hardcoded FastAPI URL with configurable setting
    - Include TypeORM cheat sheet for development reference

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,10 @@
 // src/common/common.module.ts
 import { Module } from '@nestjs/common';
 import { S3Service } from './services/s3.service';
+import { ApiClientService } from 'src/common/services/api-client.service';
 
 @Module({
-  providers: [S3Service],
-  exports: [S3Service],
+  providers: [S3Service, ApiClientService],
+  exports: [S3Service, ApiClientService],
 })
 export class CommonModule {}
